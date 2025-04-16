6312f4daa242fae7876c7a89cd0f2d421a9a20ea
Kuk1nator <132811696+Kuk1nator@users.noreply.github.com>
2025-04-16 20:00:39 +0600

    Add files via upload

---
+++ MazeSolver.java
@@ -0,0 +1,11 @@
+public final class MazeSolver {
+    private int rows;
+    private int cols;
+    private char[][] maze;
+    private List<int[]> path;
+    private int startRow;
+    private int startCol;
+    private int endRow;
+    private int endCol;
+    private final Random random = new Random();
+}
